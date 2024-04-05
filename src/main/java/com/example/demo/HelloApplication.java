fea66f3fe8010e17f67c3e9211df82bdb9bcbdb7
KHmamoune <108459491+KHmamoune@users.noreply.github.com>
2024-04-05 17:55:18 +0100

    more interfaces

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Gestion-Personnel.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Reception.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Medical Laboratory");
         stage.setResizable(false);
